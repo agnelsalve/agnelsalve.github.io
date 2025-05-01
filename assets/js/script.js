cb17a8e4d399d8ba0b9cc3efe95002fb83afc5c5
Agnel <agnel@gmail.com>
2025-05-01 13:56:18 +0530

    changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -59,7 +59,7 @@ $(document).ready(function () {
 document.addEventListener('visibilitychange',
     function () {
         if (document.visibilityState === "visible") {
-            document.title = "Portfolio | Jigar Sable";
+            document.title = "Portfolio | Agnel Salve";
             $("#favicon").attr("href", "assets/images/favicon.png");
         }
         else {
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["frontend development", "backend development", "web designing", "android development", "web development"],
+    strings: ["data analytics", "data engineering", "data science", "business intelligience", "cloud solution architect"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
